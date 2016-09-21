54bba764b8adc860c3ea3ea7784fbae8a5ee59b1
Mateusz Wenus <mateusz.wenus@gmail.com>
2016-09-21 09:53:07 +0200

    test: konstruktor rzuca NPE gdy medicinePump = null

--- src/test/java/pl/pragmatists/trainings/medicinedosekata/MedicineDosingTest.java
+++ src/test/java/pl/pragmatists/trainings/medicinedosekata/MedicineDosingTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.mock;
 import org.junit.Test;
 
 import pl.pragmatists.trainings.medicinedosekata.dependencies.AlertService;
+import pl.pragmatists.trainings.medicinedosekata.dependencies.HealthMonitor;
 import pl.pragmatists.trainings.medicinedosekata.dependencies.MedicinePump;
 
 public class MedicineDosingTest {
@@ -18,6 +19,20 @@ public class MedicineDosingTest {
 		try {
 			// when
 			new DoseController(null, medicinePump, alertService);
+			fail();
+		} catch (NullPointerException expected) {
+			// then
+		}
+    }
+    
+    @Test
+    public void DoseController_should_throw_NPE_when_medicinePump_is_null() {
+    	// given
+    	HealthMonitor healthMonitor = mock(HealthMonitor.class);
+		AlertService alertService = mock(AlertService.class);
+		try {
+			// when
+			new DoseController(healthMonitor, null, alertService);
 			fail();
 		} catch (NullPointerException expected) {
 			// then
