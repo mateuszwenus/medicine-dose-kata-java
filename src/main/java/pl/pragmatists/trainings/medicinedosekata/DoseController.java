fb76eb2302a39e5601dc05bbb02d126c1ce96b51
Mateusz Wenus <mateusz.wenus@gmail.com>
2016-09-21 10:10:59 +0200

    test przechodzi: dla cisnienia ponizej 90 powinien podac 1 dawke

--- src/main/java/pl/pragmatists/trainings/medicinedosekata/DoseController.java
+++ src/main/java/pl/pragmatists/trainings/medicinedosekata/DoseController.java
@@ -2,24 +2,27 @@ package pl.pragmatists.trainings.medicinedosekata;
 
 import pl.pragmatists.trainings.medicinedosekata.dependencies.AlertService;
 import pl.pragmatists.trainings.medicinedosekata.dependencies.HealthMonitor;
+import pl.pragmatists.trainings.medicinedosekata.dependencies.Medicine;
 import pl.pragmatists.trainings.medicinedosekata.dependencies.MedicinePump;
 
 public class DoseController {
 
+	private final MedicinePump medicinePump;
+	
     public DoseController(HealthMonitor healthMonitor, MedicinePump medicinePump, AlertService alertService) {
     	checkNotNull(healthMonitor, "healthMonitor");
-    	checkNotNull(medicinePump, "medicinePump");
+    	this.medicinePump = checkNotNull(medicinePump, "medicinePump");
     	checkNotNull(alertService, "alertService");
     }
     
-    private void checkNotNull(Object obj, String variableName) {
+    private <T> T checkNotNull(T obj, String variableName) {
     	if (obj == null) {
     		throw new NullPointerException(variableName + " must not be null");
     	}
+    	return obj;
     }
 
     public void checkHealthAndApplyMedicine() {
-
+    	medicinePump.dose(Medicine.PRESSURE_RAISING_MEDICINE);
     }
-
 }
